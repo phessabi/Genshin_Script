6bca2c623493850588f54b225f574a38436f20eb
phessabi <parham.hessabi@gmail.com>
2022-03-19 22:05:38 +0330

    fix Script

--- running.py
+++ running.py
@@ -1,6 +1,6 @@
 import keyboard as kb
 from pynput import keyboard
-from pynput.keyboard import Controller, Key
+from pynput.keyboard import Controller
 import time
 
 print("Genshin Efficient Running Script!")
@@ -9,38 +9,38 @@ K = Controller()
 
 SPRINT = False
 RUN = True
-	
 
-def run(arg):
-	while arg[0]:
-		while arg[1]:
-			K.press('s')
-			K.release('s')
-			time.sleep(0.5)
-		
-	
+
+def run():
+    while RUN:
+        if SPRINT:
+            K.press('s')
+            K.release('s')
+            time.sleep(0.5)
+
+
 def saj(key):
-	global RUN, SPRINT
-	if key == keyboard.Key.esc:		
-		RUN = False
-		return False
-	try:
-		k = key.char
-	except:
-		k = key.name
-	
-	if k == '[':
-		print('start')
-		SPRINT = True
-	
-	if k == ']':
-		print('end')
-		SPRINT = False
-	
+    global RUN, SPRINT
+    if key == keyboard.Key.esc:
+        RUN = False
+        return False
+    try:
+        k = key.char
+    except:
+        k = key.name
+
+    if k == '[':
+        print('start')
+        SPRINT = True
+
+    if k == ']':
+        print('end')
+        SPRINT = False
+
 
 from threading import Thread
 
-t = Thread(target=run, args=[RUN, SPRINT])
+t = Thread(target=run)
 t.start()
 
 listener = keyboard.Listener(on_press=saj)
@@ -48,6 +48,3 @@ listener.start()
 listener.join()
 t.join()
 
-
-
-	

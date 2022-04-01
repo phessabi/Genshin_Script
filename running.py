1223026ecfdf4a87597b48bdfc092138071b3593
TheAZed <zohri.alireza@gmail.com>
2022-04-02 01:03:44 +0430

    Refactor code

--- running.py
+++ running.py
@@ -1,7 +1,8 @@
-import keyboard as kb
+import time
+from threading import Thread
+
 from pynput import keyboard
 from pynput.keyboard import Controller
-import time
 
 print("Genshin Efficient Running Script!")
 
@@ -19,14 +20,15 @@ def run():
             K.release(keyboard.Key.shift_l)
             time.sleep(0.8)
 
-def saj(key):
+
+def run_control_callback(key):
     global RUN, SPRINT
     if key == keyboard.Key.shift_r:
         RUN = False
         return False
     try:
         k = key.char
-    except:
+    except AttributeError:
         k = key.name
 
     if k == '[':
@@ -38,14 +40,11 @@ def saj(key):
         SPRINT = False
 
 
-from threading import Thread
-
 t = Thread(target=run)
 t.start()
 
-listener = keyboard.Listener(on_press=saj)
+listener = keyboard.Listener(on_press=run_control_callback)
 listener.start()
 print('Ready!')
 listener.join()
 t.join()
-
